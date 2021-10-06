e45256a6a4037e430d70e4b0722443b75b4bd51b
巧庭 <u3814520@gmail.com>
2021-10-06 09:53:43 +0800

    fix10/06

--- Video_project/pipeline/steps/download_caption.py
+++ Video_project/pipeline/steps/download_caption.py
@@ -7,7 +7,7 @@ class DownloadCaptions(Step):
             source = YouTube(url)
             en_caption = source.captions.get_by_language_code('a.en')  # a代表自動產生
             en_caption_convert_to_srt = (en_caption.generate_srt_captions())
-            print(en_caption_convert_to_srt)
+            print(str(en_caption_convert_to_srt))
 
             # save the caption to a file named Output.txt
             text_file = open(utils.get_video_id(url), "w")
